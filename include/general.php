5a0dab106ffab0c0ce64ec2fa4814a414cdefdc6
dhuby <dhuby@montala.com>
2016-12-09 11:15:59 +0000

    FIX: Rv last commit as it is incomplete and already patched in trunk/8.0rc anyway

--- include/general.php
+++ include/general.php
@@ -3380,10 +3380,8 @@ function check_access_key($resource,$key)
 	
 	# Option to plugin in some extra functionality to check keys
 	if (hook("check_access_key","",array($resource,$key))===true) {return true;}
-
-
-	global $external_share_view_as_internal;
-    	if($external_share_view_as_internal && isset($_COOKIE["user"]) && validate_user("u.session='" . escape_check($_COOKIE["user"]) . "'",false)) {return false;} 
+	global $external_share_view_as_internal, $is_authenticated;
+    	if($external_share_view_as_internal && (isset($_COOKIE["user"]) && !(isset($is_authenticated) && $is_authenticated))){return false;} // We want to authenticate the user if not already authenticated so we can show the page as internal
 	
 	$keys=sql_query("select user,usergroup,expires from external_access_keys where resource='$resource' and access_key='$key' and (expires is null or expires>now())");
 
@@ -3520,7 +3518,7 @@ function check_access_key_collection($collection, $key)
         }
     
     global $external_share_view_as_internal;
-    if($external_share_view_as_internal && isset($_COOKIE["user"]) && validate_user("u.session='" . escape_check($_COOKIE["user"]) . "'",false))
+    if($external_share_view_as_internal && isset($_COOKIE["user"]))
         {
         // We want to authenticate the user so we can show the page as internal
         return false;
@@ -5583,5 +5581,3 @@ function user_set_usergroup($user,$usergroup)
     {
     sql_query("update user set usergroup='" . escape_check($usergroup) . "' where ref='" . escape_check($user) . "'");
     }
-
-
